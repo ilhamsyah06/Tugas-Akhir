0a3b2373f012adcec449a90c64620fb8fbb195cf
ilhamsyah06 <droixsgame@gmail.com>
2020-04-30 14:17:36 +0700

    fix bug

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+            \App\User::create([
+            'name'  => 'Admin',
+             'email' => 'admin@admin.com',
+             'password'  => bcrypt('admin'),
+             'alamat' => 'ds Brengolo',
+             'nomorhp' => '08578998832',
+             'level' => 'admin'
+        ]);
+
     }
 }
